8bbcb49f331364d070048fd8bf734c53c2ceb432
Valentina Danilova <valentinka-danilova@mail.ru>
2020-07-15 12:52:12 +0300

    added markdown plugin as a dependency

--- src/main/java/org/intellij/plugin/mdx/lang/psi/MDXElementType.java
+++ src/main/java/org/intellij/plugin/mdx/lang/psi/MDXElementType.java
@@ -3,6 +3,8 @@ package org.intellij.plugin.mdx.lang.psi;
 import com.intellij.psi.tree.IElementType;
 import org.intellij.plugin.mdx.lang.MDXLanguage;
 import org.jetbrains.annotations.NotNull;
+import org.intellij.markdown.*;
+import org.intellij.plugins.markdown.*;
 
 public class MDXElementType extends IElementType {
 
