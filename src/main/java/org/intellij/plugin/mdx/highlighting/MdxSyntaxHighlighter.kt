daf7606763f424f03b0ef1a0b9277f3de06cbb6b
Valentina Danilova <valentinka-danilova@mail.ru>
2020-07-28 11:36:05 +0300

    fixed MET bugs

--- src/main/java/org/intellij/plugin/mdx/highlighting/MdxSyntaxHighlighter.kt
+++ src/main/java/org/intellij/plugin/mdx/highlighting/MdxSyntaxHighlighter.kt
@@ -1,12 +1,17 @@
 package org.intellij.plugin.mdx.highlighting
 
+import com.intellij.lexer.Lexer
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase
+import org.intellij.plugin.mdx.lang.parse.MdxLexer
 import org.intellij.plugin.mdx.lang.parse.MdxTokenTypes
 import org.intellij.plugins.markdown.highlighting.MarkdownHighlighterColors
 import org.intellij.plugins.markdown.highlighting.MarkdownSyntaxHighlighter
 import org.intellij.plugins.markdown.lang.MarkdownElementType
 
 class MdxSyntaxHighlighter : MarkdownSyntaxHighlighter() {
+    override fun getHighlightingLexer(): Lexer {
+        return MdxLexer()
+    }
     companion object {
         init {
             SyntaxHighlighterBase.safeMap(
--- src/main/java/org/intellij/plugin/mdx/lang/parse/MdxElementType.kt
+++ src/main/java/org/intellij/plugin/mdx/lang/parse/MdxElementType.kt
@@ -6,15 +6,21 @@ import org.intellij.plugins.markdown.lang.MarkdownElementType
 import org.jetbrains.annotations.Contract
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.Nullable
+import java.util.*
 
 class MdxElementType(@NonNls debugName: String) : IElementType(debugName, MdxLanguage.INSTANCE) {
     companion object {
+        private val mdxToPlatformTypeMap: HashMap<org.intellij.markdown.IElementType, IElementType> = HashMap()
         @Contract("null -> null; !null -> !null")
         @Synchronized
         fun platformType(@Nullable markdownType: org.intellij.markdown.IElementType?): IElementType {
             val result: IElementType
             if (markdownType == MdxTokenTypes.JSX_BLOCK_CONTENT || markdownType == MdxElementTypes.JSX_BLOCK || markdownType == MdxElementTypes.MDX_FILE) {
+                if (mdxToPlatformTypeMap.containsKey(markdownType)) {
+                    return mdxToPlatformTypeMap[markdownType]!!
+                }
                 result = MdxElementType(markdownType.toString())
+                mdxToPlatformTypeMap[markdownType] = result
                 return result;
             }
             result = MarkdownElementType.platformType(markdownType);
